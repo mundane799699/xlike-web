27183003e87bd215d862d46e99efe7744fbfeba0
mundane <799699348@qq.com>
2024-10-02 21:54:13 +0800

    小小修改一下布局

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,15 +7,17 @@ export const dynamic = "force-dynamic";
 // See https://shipfa.st/docs/tutorials/private-page
 export default async function Dashboard() {
   return (
-    <main className="min-h-screen pt-16">
+    <div className="min-h-screen pt-16">
       <header className="fixed top-0 left-0 right-0 bg-white z-10 p-4 flex justify-end shadow-md">
         <ButtonAccount />
       </header>
-      <section className="max-w-full mx-auto space-y-8 p-8 ">
-        <h1 className="text-3xl md:text-4xl font-extrabold h-[1080px] bg-pink-300">
-          Private Page
-        </h1>
-      </section>
-    </main>
+      <main className="p-8">
+        <section className="max-w-full mx-auto ">
+          <h1 className="text-3xl md:text-4xl font-extrabold h-[1080px] bg-pink-300">
+            Private Page
+          </h1>
+        </section>
+      </main>
+    </div>
   );
 }
